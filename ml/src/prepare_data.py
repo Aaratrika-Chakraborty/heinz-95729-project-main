750aa3823c6db92e62d2eb3f4adf73ab4304d7b1
wxp67 <95328938+wxp67@users.noreply.github.com>
2021-12-10 20:15:15 -0500

    Update prepare_data.py

--- ml/src/prepare_data.py
+++ ml/src/prepare_data.py
@@ -10,7 +10,6 @@ def read_data(filename):
     data = data[data['res_id'] != '-1'].dropna(how='any', axis=0)
     return data
 
-
 def gen_score(data):
     lis = []
     i = 0
@@ -23,20 +22,19 @@ def gen_score(data):
     final_data = pd.concat(lis)
     return final_data
 
-
 if __name__ == "__main__":
-    data1 = read_data('/Users/lemonvn/Downloads/entree/session/session.1996-Q3')
-    data2 = read_data('/Users/lemonvn/Downloads/entree/session/session.1996-Q4')
-    data3 = read_data('/Users/lemonvn/Downloads/entree/session/session.1997-Q1')
-    data4 = read_data('/Users/lemonvn/Downloads/entree/session/session.1997-Q2')
-    data5 = read_data('/Users/lemonvn/Downloads/entree/session/session.1997-Q3')
-    data6 = read_data('/Users/lemonvn/Downloads/entree/session/session.1997-Q4')
-    data7 = read_data('/Users/lemonvn/Downloads/entree/session/session.1998-Q1')
-    data8 = read_data('/Users/lemonvn/Downloads/entree/session/session.1998-Q2')
-    data9 = read_data('/Users/lemonvn/Downloads/entree/session/session.1998-Q3')
-    data10 = read_data('/Users/lemonvn/Downloads/entree/session/session.1998-Q4')
-    data11 = read_data('/Users/lemonvn/Downloads/entree/session/session.1999-Q1')
-    data12 = read_data('/Users/lemonvn/Downloads/entree/session/session.1999-Q2')
+    data1 = read_data('data/session/session.1996-Q3')
+    data2 = read_data('data/session/session.1996-Q4')
+    data3 = read_data('data/session/session.1997-Q1')
+    data4 = read_data('data/session/session.1997-Q2')
+    data5 = read_data('data/session/session.1997-Q3')
+    data6 = read_data('data/session/session.1997-Q4')
+    data7 = read_data('data/session/session.1998-Q1')
+    data8 = read_data('data/session/session.1998-Q2')
+    data9 = read_data('data/session/session.1998-Q3')
+    data10 = read_data('data/session/session.1998-Q4')
+    data11 = read_data('data/session/session.1999-Q1')
+    data12 = read_data('data/session/session.1999-Q2')
 
     data = pd.concat([data1, data2, data3, data4, data5, data6, data7, data8, data9, data10, data11, data12])
     data = pd.DataFrame({'count': data.groupby(['user_name', 'res_id']).res_id.count()}).reset_index()
@@ -44,4 +42,3 @@ if __name__ == "__main__":
 
     data = gen_score(data)
     data.to_csv("../res/session_data_concat.csv", index=False)
-    print(1)
